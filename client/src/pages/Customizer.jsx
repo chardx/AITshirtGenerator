747d5843d3ff4639d73ecfcafc450171b62978b1
Richard Roxas <chardsourcex@gmail.com>
2023-05-18 17:40:15 +0800

    fix deployment vercel

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -61,15 +61,18 @@ const Customizer = () => {
     try {
       //call our backend to generate an ai image!
       setGeneratingImg(true);
-      const response = await fetch("http://localhost:8080/api/v1/dalle", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          prompt,
-        }),
-      });
+      const response = await fetch(
+        "https://project-threejs-ai-eluc.onrender.com/api/v1/dalle",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            prompt,
+          }),
+        }
+      );
 
       const data = await response.json();
 
